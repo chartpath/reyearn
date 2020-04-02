188fb37c499f762ef9df8124043ddc65ec7a605a
Chris Hart <chrstphrhrt@gmail.com>
2020-04-02 19:29:49 -0400

    add annotation validtion in api

--- api/handlers.py
+++ api/handlers.py
@@ -1,4 +1,6 @@
+from typing import List
 from fastapi import APIRouter, Request
+from pydantic import BaseModel
 from db import schemas
 
 router = APIRouter()
@@ -9,18 +11,29 @@ async def root():
     return {"ack": "ok"}
 
 
-@router.get("/annotations")
+class AnnoIn(BaseModel):
+    class_id: int
+    observation_id: int
+
+
+class Anno(BaseModel):
+    id: int
+    class_id: int
+    observation_id: int
+
+
+@router.get("/annotations", response_model=List[Anno])
 async def read_annotations(req: Request):
     db = req.app.state.db_client
     annotations = await db.fetch_all(schemas.annotations.select())
     return annotations
 
 
-@router.post("/annotations")
-async def create_annotation(anno: dict, req: Request):
+@router.post("/annotations", response_model=Anno)
+async def create_annotation(anno: AnnoIn, req: Request):
     db = req.app.state.db_client
     query = schemas.annotations.insert().values(
-        class_id=anno["class_id"], observation_id=anno["observation_id"]
+        class_id=anno.class_id, observation_id=anno.observation_id
     )
     last_record_id = await db.execute(query)
-    return {**anno, "id": last_record_id}
+    return {**anno.dict(), "id": last_record_id}
