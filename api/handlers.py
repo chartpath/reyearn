7eb512c6559782df47b4580ebc89594b197a30a9
Chris Hart <chrstphrhrt@gmail.com>
2020-04-02 19:40:34 -0400

    cleans up validation, adds observations endpoint

--- api/handlers.py
+++ api/handlers.py
@@ -11,29 +11,53 @@ async def root():
     return {"ack": "ok"}
 
 
-class AnnoIn(BaseModel):
+class AnnotationCreate(BaseModel):
     class_id: int
     observation_id: int
 
 
-class Anno(BaseModel):
+class AnnotationRead(BaseModel):
     id: int
     class_id: int
     observation_id: int
 
 
-@router.get("/annotations", response_model=List[Anno])
+@router.get("/annotations", response_model=List[AnnotationRead])
 async def read_annotations(req: Request):
     db = req.app.state.db_client
     annotations = await db.fetch_all(schemas.annotations.select())
     return annotations
 
 
-@router.post("/annotations", response_model=Anno)
-async def create_annotation(anno: AnnoIn, req: Request):
+@router.post("/annotations", response_model=AnnotationRead)
+async def create_annotation(anno: AnnotationCreate, req: Request):
     db = req.app.state.db_client
     query = schemas.annotations.insert().values(
         class_id=anno.class_id, observation_id=anno.observation_id
     )
     last_record_id = await db.execute(query)
     return {**anno.dict(), "id": last_record_id}
+
+
+class ObservationCreate(BaseModel):
+    text: str
+
+
+class ObservationRead(BaseModel):
+    id: int
+    text: str
+
+
+@router.get("/observations", response_model=List[ObservationRead])
+async def read_observations(req: Request):
+    db = req.app.state.db_client
+    annotations = await db.fetch_all(schemas.observations.select())
+    return annotations
+
+
+@router.post("/observations", response_model=ObservationRead)
+async def create_observations(obs: ObservationCreate, req: Request):
+    db = req.app.state.db_client
+    query = schemas.observations.insert().values(text=obs.text)
+    last_record_id = await db.execute(query)
+    return {**obs.dict(), "id": last_record_id}
