579944e8c15630ae33639ed06009c9c91cc80b00
Chris Hart <chrstphrhrt@gmail.com>
2020-04-02 03:06:51 -0400

    moves test path fix

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,3 @@
-import os, sys
-from os.path import dirname, join, abspath
-
-sys.path.insert(0, abspath(join(dirname(__file__), "..")))
-
 from ..server import app
 from starlette.testclient import TestClient
 
